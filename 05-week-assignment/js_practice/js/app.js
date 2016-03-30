049281f55ee27044d784fa72f86174cdf6437bac
alexyuu <alex.cm.yu@gmail.com>
2016-03-29 21:45:49 -0700

    week 5 class project

---
+++ 05-week-assignment/js_practice/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+  // alert("Wait, don't leave. Gimme $$$")
+  //
+  // $("h1").click(
+  //     function(){
+  //       alert("You just clicked")
+  //     }
+  //   )
+
+  // var color = prompt("What color?")
+  // console.log(color)
+  //
+  // color = "black"
+  // console.log(color)
+
+  var firstNum = 1
+  var secondNum = 1
+  var answer = firstNum * secondNum
+  console.log("Your total is: ")
+  console.log(answer)
+
+  if( answer == 1 ){
+      console.log("Woohoo! The answer is one!")
+
+  }
+
+  var lightSwitch = false
+
+  if( lightSwitch == true ){
+    alert("The lights are on")
+  }else{
+    alert("The lights are off")
+  }
+
+});
